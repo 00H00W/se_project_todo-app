4840c0dedec5cda5878d7eec8f804839d29be9bf
Sam Branham <hotdogsb@gmail.com>
2024-11-21 09:27:13 -0600

    refactor: move form validation to class

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 // Internal
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
+import FormValidator from "../components/FormValidator.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -52,3 +53,6 @@ initialTodos.forEach((item) => {
   const todo = generateTodo(item);
   todosList.append(todo);
 });
+
+const newTodoValidator = new FormValidator(addTodoForm, validationConfig);
+newTodoValidator.enableValidation();
