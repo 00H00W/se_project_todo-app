8a9e90ff1717f8de21c21fadc9901c06064392bf
Sam Branham <hotdogsb@gmail.com>
2024-11-21 07:31:38 -0600

    fix: new todos generate id

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+// External
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+// Internal
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 
@@ -33,12 +36,13 @@ addTodoForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const name = evt.target.name.value;
   const dateInput = evt.target.date.value;
+  const id = uuidv4();
 
   // Create a date object and adjust for timezone
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
-  const values = { name, date };
+  const values = { name, date, id };
   const todo = generateTodo(values);
   todosList.append(todo);
   closeModal(addTodoPopup);
