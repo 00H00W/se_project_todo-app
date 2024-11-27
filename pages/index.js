54dcb87ac71884c4d845646f4043a98bc891b8d7
Sam Branham <hotdogsb@gmail.com>
2024-11-27 05:32:31 -0600

    refactor: add section class

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,22 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
+import Section from "../utils/Section.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
+
+const section = new Section({
+  items: initialTodos,
+  renderer: (item) => {
+    const todo = generateTodo(item);
+    todosList.append(todo);
+  },
+  containerSelector: ".todos__list",
+});
 
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
@@ -19,11 +29,11 @@ const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
 
-const generateTodo = (data) => {
+function generateTodo(data) {
   const todo = new Todo(data, "#todo-template");
   const todoElement = todo.getView();
   return todoElement;
-};
+}
 
 addTodoButton.addEventListener("click", () => {
   openModal(addTodoPopup);
@@ -45,14 +55,9 @@ addTodoForm.addEventListener("submit", (evt) => {
 
   const values = { name, date, id };
   const todo = generateTodo(values);
-  todosList.append(todo);
+  section.addItem(todo);
   closeModal(addTodoPopup);
   newTodoValidator.resetValidation();
-});
-
-initialTodos.forEach((item) => {
-  const todo = generateTodo(item);
-  todosList.append(todo);
 });
 
 const newTodoValidator = new FormValidator(addTodoForm, validationConfig);
---
+++ utils/Section.js
@@ -0,0 +1,20 @@
+class Section {
+  constructor({ items, renderer, containerSelector }) {
+    this._items = items;
+    this._renderer = renderer;
+    this._containerElement = document.querySelector(containerSelector);
+
+    this.renderItems();
+  }
+
+  renderItems() {
+    this._items.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element) {
+    this._containerElement.append(element);
+  }
+}
+export default Section;
